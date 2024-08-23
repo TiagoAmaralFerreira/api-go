190abb957e6b2f060cd29ecb364a5f01ba58e700
Tiago <tiago.amaral2901@gmail.com>
2024-08-22 21:24:25 -0300

    feat: Create teste product and struc User_db

--- internal/entity/product_test.go
+++ internal/entity/product_test.go
@@ -27,8 +27,14 @@ func TestProductWhenPriceIsRequired(t *testing.T) {
 	assert.Equal(t, ErrPriceIsRequired, err)
 }
 
+func TestProductWhenPriceIsInvalid(t *testing.T) {
+	p, err := NewProduct("Product 1", -10)
+	assert.Nil(t, p)
+	assert.Equal(t, ErrInvalidPrice, err)
+}
+
 func TestProductValidate(t *testing.T) {
-	p, err := NewProduct("Produto 1", 20)
+	p, err := NewProduct("Product 1", 10)
 	assert.Nil(t, err)
 	assert.NotNil(t, p)
 	assert.Nil(t, p.Validate())
